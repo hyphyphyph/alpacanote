40d656b744ecae906d8489ff3bc5f495f0c952c6
Derek McTavish Mounce <derek@mounce.wtf>
2016-03-04 01:09:39 -0500

    Oops.  Forgot to actually add libpgp.  Generate keys if they don't exist on server start.

---
+++ libpgp/tests/test-generateKeys.js
@@ -0,0 +1,57 @@
+import Chai from 'chai';
+import Fs from 'fs';
+import LibPgp from '../index';
+
+describe('LibPgp.generateKeys', () => {
+  var publicKey;
+  var privateKey;
+  var encryptedMessage;
+
+  it('Should be able to generateKeys', function (done) {
+    // This could take a while...
+    this.timeout(3600000);
+
+    new LibPgp().generateKeys()
+      .then((key) => {
+        Chai.expect(key).to.be.an.object;
+        Chai.expect(key.publicKey).to.be.a.string;
+        Chai.expect(key.privateKey).to.be.a.string;
+
+        publicKey = key.publicKey;
+        privateKey = key.privateKey;
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('Should be able to encryptMessageWithPublicKey', (done) => {
+    new LibPgp().encryptMessageWithPublicKey('Hello World', publicKey)
+      .then((result) => {
+        Chai.expect(result).to.be.a.string;
+
+        encryptedMessage = result;
+
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it('Should be able to decryptMessageWithPrivateKey', (done) => {
+    new LibPgp().decryptMessageWithPrivateKey(encryptedMessage, privateKey)
+      .then((result) => {
+        Chai.expect(result).to.be.a.string;
+        Chai.expect(result).to.equal('Hello World');
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+});
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-import * as Hapi from 'hapi';
+import Fs from 'fs';
+import Hapi from 'hapi';
+
+import LibPgp from '../libpgp';
 
 import Server from './server';
 import Config from './config';
 
-var server = new Server({
-  hostname: Config.Server.Hostname,
-  port: Config.Server.Port
-});
+var publicKeyExists = Fs.existsSync('public.key');
+var privateKeyExists = Fs.existsSync('private.key');
+if (!publicKeyExists || !privateKeyExists) {
+  console.log('Generating Keys... Please wait.');
+  new LibPgp()
+    .generateKeys()
+    .then((key) => {
+      Fs.writeFileSync('public.key', key.publicKey);
+      Fs.writeFileSync('private.key', key.privateKey);
+      initServer();
+    });
+}
+else {
+  initServer();
+}
 
-server.init(() => {
-  server.start();
-});
+function initServer () {
+  var server = new Server({
+    hostname: Config.Server.Hostname,
+    port: Config.Server.Port
+  });
+
+  server.init(() => {
+    server.start();
+  });
+}
 
